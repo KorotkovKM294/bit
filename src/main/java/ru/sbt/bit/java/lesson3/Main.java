838f78c122b091f366c25c02caedb377154bdcce
KorotkovKM294 <darkinalt@gmail.com>
2016-10-10 17:07:44 +0300

    Initial commit.

---
+++ src/main/java/ru/sbt/bit/java/lesson3/Main.java
@@ -0,0 +1,14 @@
+package ru.sbt.bit.java.lesson3;
+
+import java.util.*;
+
+/**
+ * Created by Cyril Korotkov on 10.10.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<String>();
+        List<String> linkedList = new LinkedList<String>();
+        Map<String, Integer> hashMap = new HashMap<String, Integer>();
+    }
+}
