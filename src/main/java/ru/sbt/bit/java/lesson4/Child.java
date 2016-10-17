ff2194c577d2d5f508c8e2b5863bdfce7ea5b777
KorotkovKM294 <darkinalt@gmail.com>
2016-10-17 20:57:48 +0300

    fix

--- src/main/java/ru/sbt/bit/java/lesson4/Child.java
+++ src/main/java/ru/sbt/bit/java/lesson4/Child.java
@@ -1,7 +1,7 @@
 package ru.sbt.bit.java.lesson4;
 
 /**
- * Created by KIDCLASS on 17.10.2016.
+ * Created by Cyril Korotkov on 17.10.2016.
  */
 public class Child extends Person {
     public Child(String name) {
